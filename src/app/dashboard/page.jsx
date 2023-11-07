a1a63b1b09e1b488ecbc296da016effffe8a8e10
Faraz Khan <farazkhanaa2555@gmail.com>
2023-11-07 21:49:21 +0500

    Clean Code

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,29 +7,6 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const Dashboard = () => {
-  // const [data, setData] = useState([]);
-  // const [err, setErr] = useState(false);
-  // const [isLoading, setLoading] = useState(false);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     setLoading(true);
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-  //       cache: "no-store",
-  //     });
-
-  //     if (!res.ok) {
-  //       setErr(true);
-  //     }
-
-  //     const data = await res.json();
-
-  //     setData(data);
-  //     setLoading(false);
-  //   };
-  //   getData();
-  // }, []);
-
   const session = useSession();
 
   const router = useRouter();
