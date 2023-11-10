745df5ae7523eba4acf80e09d4b88796feea7185
Faraz Khan <farazkhanaa2555@gmail.com>
2023-11-10 15:22:51 +0500

    updated

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import connect from "@/utils/db";
-import User from "@/models/User";
+import connect from "../../../../utils/db";
+import User from "../../../../models/User";
 import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
